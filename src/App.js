865104bb6a05eb9b6c16ab6681a5cfe7c20c8ae8
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-09-05 18:43:53 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
-        crossOrigin="anonymous"
-      />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat&display=swap"
-        rel="stylesheet"
-      />
-      <SearchForm />
-      <WeatherInfo />
-      <WeatherForecast/>
-      <Footer />
+    <div className="container">
+      <div className="App">
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+          crossOrigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
+        <SearchForm />
+        <WeatherInfo />
+        <WeatherForecast/>
+        <Footer />
+    </div>
   </div>
   );
 }
